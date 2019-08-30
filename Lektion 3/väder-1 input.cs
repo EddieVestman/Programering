2d6345db4330f6cca77547dbd78227538305f972
EddieVestman <33714818+EddieVestman@users.noreply.github.com>
2019-08-30 09:05:03 +0200

    La till resten av uppgiften

--- Lektion 3/väder-1 input.cs
+++ Lektion 3/väder-1 input.cs
@@ -6,5 +6,9 @@ class MainClass {
     string svar = Console.ReadLine();
     if (svar == "ja" ||  svar == "Ja")
       Console.WriteLine("Vi går på picknick!");
+    else if (svar == "nej" || svar == "Nej")
+      Console.WriteLine("Vi Stannar inne och läser en bok");
+    else
+      Console.WriteLine ("Jag förstår inte");
   }
 }
